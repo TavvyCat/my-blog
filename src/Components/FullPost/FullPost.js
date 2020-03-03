8f5a3b944704ce97b58944554a42b552fc37ae88
Tavish <tavish.cary@welch.edu>
2020-03-03 16:10:20 -0600

    Update FullPost.js

--- src/Components/FullPost/FullPost.js
+++ src/Components/FullPost/FullPost.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import Axios from '../../Axios';
+import { storage } from '../../Firebase/Firebase';
 import Paragraph from '../UI/Paragraph/Paragraph';
 
 class FullPost extends Component {
+  constructor(props) {
+    super(props);
+    this.getImages();
+  }
+
   state = {
-    images: []
+    imgURL: ""
   }
 
-  componentDidMount() {
-    console.log(this.props.location.aboutProps);
+  getImages() {
+    storage.ref(this.props.imgFile).child(this.props.imgName).getDownloadURL()
+    .then(response => {
+      this.setState({imgURL: response});
+    })
+    .catch(error => console.log(error));
   }
 
-  render () {
-    console.log(this.props.location.aboutProps)
+  render() {
     return (
-      <p>FullPost</p>
+      <div className="BlogPreview">
+        <div>
+          <img src={this.state.imgURL} alt="" />
+        </div>
+        <h3>{this.props.title}</h3>
+        <p className="line-clamp">{this.props.content}</p>
+        <h4>Posted {this.props.date}</h4>
+      </div>
     )
   }
 }
