91e2269a695c5f2ff39f6e9f7da44610a312e377
TavvyCat <56958408+TavvyCat@users.noreply.github.com>
2020-03-03 12:41:17 -0800

    Create BlogPreview.js

---
+++ src/Components/BlogPreview/BlogPreview.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { storage } from '../../Firebase/Firebase';
+import './BlogPreview.css';
+
+class BlogPreview extends Component {
+  constructor(props) {
+    super(props);
+    this.getImages();
+  }
+
+  state = {
+    imgURL: ""
+  }
+
+  getImages() {
+    storage.ref(this.props.imgFile).child(this.props.imgName).getDownloadURL()
+    .then(response => {
+      this.setState({imgURL: response});
+    })
+    .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="BlogPreview">
+        <div>
+          <img src={this.state.imgURL} alt="" />
+        </div>
+        <h3>{this.props.title}</h3>
+        <p className="line-clamp">{this.props.content}</p>
+        <h4>Posted {this.props.date}</h4>
+      </div>
+    )
+  }
+}
+
+export default BlogPreview;
