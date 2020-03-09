4f6e871d82f9d4477e294082e26b678f0de01ad0
Tavish <tavish.cary@welch.edu>
2020-03-09 14:45:07 -0500

    Update FullPost.js

--- src/Components/FullPost/FullPost.js
+++ src/Components/FullPost/FullPost.js
@@ -1,30 +1,61 @@
 import React, { Component } from 'react';
 import { storage } from '../../Firebase/Firebase';
 import Aux from '../HOC/AuxComp';
-import Paragraph from '../UI/Paragraph/Paragraph';
 
-class FullPost extends Component {
-  constructor(props) {
-    super(props);
-    this.getImages();
-  }
-
+class FullPost extends Component { 
   state = {
-    imgURL: ""
+    data: null,
+    imgURLs: null
   }
 
-  getImages() {
-    storage.ref(this.props.imgFile).child(this.props.imgName).getDownloadURL()
-    .then(response => {
-      this.setState({imgURL: response});
-    })
-    .catch(error => console.log(error));
+  componentDidMount () {
+    console.log("Component did mount");
+    this.getData();
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (nextProps !== this.props) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  componentDidUpdate (nextProps, nextState) {
+    console.log("component did update");
+    if (this.state !== nextState) {
+      this.getData();
+    }
+  }
+
+  async getData () {
+    const obj = this.props.location.state.images;
+    const images = new Array(0);
+    const dataState = {...this.props.location.state};
+    for (let key in obj) {
+      await storage.ref(obj[key].file).child(obj[key].imageName).getDownloadURL()
+      .then(response => {
+        images.push(response);
+      })
+      .catch(error => console.log(error));
+    }
+    this.setState({data: dataState, imgURLs: images});
   }
 
   render() {
+    let post = this.state.imgURLs && this.state.data ? (
+      <Aux>
+        <img src={this.state.imgURLs.shift()} alt="" style={{width: "50vw", height: "auto", marginTop: "20px"}}/>
+        <h2 style={{fontSize: "32px", fontFamily: "cursive"}}>{this.state.data.title}</h2>
+        <h5>{this.state.data.date}</h5>
+        <p style={{lineHeight: "2", margin: "20px"}}>{this.state.data.content}</p> 
+        <h1>More Posts</h1>
+      </Aux>
+    ) : null;
+
     return (
       <Aux>
-
+        {post}
       </Aux>
     )
   }
