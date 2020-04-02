cf2962833687c8cf920ed5573a826c7d278582a8
Tavish <tavish.cary@welch.edu>
2020-04-02 12:19:46 -0500

    Update BlogPreview.js

--- src/Components/BlogPreview/BlogPreview.js
+++ src/Components/BlogPreview/BlogPreview.js
@@ -1,37 +1,15 @@
-import React, { Component } from 'react';
-import { storage } from '../../Firebase/Firebase';
+import React from 'react';
 import './BlogPreview.css';
 
-class BlogPreview extends Component {
-  constructor(props) {
-    super(props);
-    this.getImages();
-  }
-
-  state = {
-    imgURL: ""
-  }
-
-  getImages() {
-    storage.ref(this.props.imgFile).child(this.props.imgName).getDownloadURL()
-    .then(response => {
-      this.setState({imgURL: response});
-    })
-    .catch(error => console.log(error));
-  }
-
-  render() {
-    return (
-      <div className="BlogPreview">
-        <div>
-          <img src={this.state.imgURL} alt="" />
-        </div>
-        <h3>{this.props.title}</h3>
-        <p className="line-clamp">{this.props.content}</p>
-        <h4>Posted {this.props.date}</h4>
-      </div>
-    )
-  }
-}
+const BlogPreview = props => (
+  <div className="BlogPreview">
+    <div>
+      <img src={props.images._titleImage} alt="" />
+    </div>
+    <h3>{props.title}</h3>
+    <p className="line-clamp">{props.content}</p>
+    <h4>Posted {props.date}</h4>
+  </div>
+)
 
 export default BlogPreview;
